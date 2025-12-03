cbeab3bf7444c09f44da7c067d6e44fcd7f69e1f
Infi <infiziert@protonmail.ch>
2025-12-03 18:54:56 +0100

    - Remove SSR = false for a test

--- Website/svelte/src/routes/+layout.ts
+++ Website/svelte/src/routes/+layout.ts
@@ -1,8 +1,6 @@
 ﻿import type { PageLoad } from "../../.svelte-kit/types/src/routes/occult/$types";
 import { loadMapping } from "$lib/loadHelpers";
 
-export const ssr = false
-
 // @ts-ignore
 export const load: PageLoad = async ({ fetch }) => {
     await loadMapping(fetch)
