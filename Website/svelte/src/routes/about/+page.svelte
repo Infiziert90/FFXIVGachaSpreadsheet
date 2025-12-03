b83c450fcddc9d01a387c9f222b37ba7dea92a5d
Infi <infiziert@protonmail.ch>
2025-12-03 18:44:08 +0100

    - Add About subpage

---
+++ Website/svelte/src/routes/about/+page.svelte
@@ -0,0 +1,28 @@
+﻿<script lang="ts">
+    import {description, title} from "$lib/title.svelte";
+    import {page} from "$app/state";
+
+    title.set("About");
+    description.set("A simple about page.");
+</script>
+
+<svelte:head>
+    <title>About</title>
+
+    <meta property="og:site_name" content="About">
+    <meta property="og:title" content="About">
+    <meta property="og:type" content="website">
+    <meta name="description" content="A simple about page." />
+    <meta property="og:description" content="A simple about page." />
+</svelte:head>
+
+<div class="col">
+    <p>The website is based on the data contributed by the community:</p>
+
+    <ul>
+        <li>Data collected through Tracky plugin</li>
+        <li>Data manually collected through "Overseas Casuals" discord server</li>
+    </ul>
+
+    <p>If you would like to contribute data, please contact Infi (@infi on Discord).</p>
+</div>
