581a6860459404103ca72779286e9d5399c5796b
Infi <infiziert@protonmail.ch>
2025-12-04 15:51:27 +0100

    - Move code to load function

--- website/src/routes/+layout.ts
+++ website/src/routes/+layout.ts
@@ -1,7 +1,21 @@
 ﻿import type { PageLoad } from "../../.svelte-kit/types/src/routes/occult/$types";
-import { loadMapping } from "$lib/loadHelpers";
+import {type ItemInfo, Mappings} from "$lib/mappings";
+import {errorHandling, logAndThrow, responseHandler} from "$lib/utils";
 
 // @ts-ignore
 export const load: PageLoad = async ({ fetch }) => {
-    await loadMapping(fetch)
+    try {
+        if (Object.keys(Mappings).length > 0) return;
+
+        await fetch('/data/Mappings.json')
+            .then(responseHandler)
+            .then((data: Record<number, ItemInfo>) =>{
+                for (const [key, value] of Object.entries(data)) {
+                    Mappings[parseInt(key)] = value;
+                }
+            })
+            .catch(errorHandling);
+    } catch (err) {
+        logAndThrow('Error loading mapping data.', err)
+    }
 }
--- website/svelte.config.js
+++ website/svelte.config.js
@@ -6,7 +6,16 @@ const config = {
     // Consult https://svelte.dev/docs/kit/integrations
     // for more information about preprocessors
     preprocess: vitePreprocess(),
-    kit: { adapter: adapter() }
+    kit: {
+        adapter: adapter({
+            // See below for an explanation of these options
+            fallback: 'plaintext',
+            routes: {
+                include: ['/*'],
+                exclude: ['<all>']
+            }
+        })
+    }
 };
 
 export default config;
