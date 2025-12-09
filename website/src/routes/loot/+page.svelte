97a9d772a5dd4d648aae6a5c6b3ce6e851c74b4a
krz <maxlav78@gmail.com>
2025-12-09 22:31:17 +0100

    Refactor LootAccordion and externalize loot tree styles

--- website/src/routes/loot/+page.svelte
+++ website/src/routes/loot/+page.svelte
@@ -164,7 +164,7 @@
             <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#offcanvasFilter" aria-label="Close"></button>
         </div>
         <div class="offcanvas-body">
-            <LootAccordionOg {chestDropData} {category} {expansion} {header} {duty} {openTab} />
+            <LootAccordion {chestDropData} {category} {expansion} {header} {duty} {openTab} />
         </div>
     </div>
 </div>
