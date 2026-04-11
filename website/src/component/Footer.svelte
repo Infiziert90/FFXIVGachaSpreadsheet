4735bdbbc92df6b4b25804073d1cff7ffa40eb67
Infi <infiziert@protonmail.ch>
2026-04-11 03:52:42 +0200

    - Add kofi link - Add discord support link

--- website/src/component/Footer.svelte
+++ website/src/component/Footer.svelte
@@ -2,5 +2,5 @@
 </script>
 
 <footer class="bg-body-tertiary text-body-tertiary text-center py-3 mt-5">
-    <p class="mb-0">Made by Infi</p>
+    <p class="mb-0 ml-5">Made by Infi <a href="https://ko-fi.com/infiii" rel="nofollow" class="mx-5 link-opacity-50 link-info">Support this site</a></p>
 </footer>
--- website/src/routes/about/+page.svelte
+++ website/src/routes/about/+page.svelte
@@ -18,5 +18,5 @@
         <li>Data manually collected through "Overseas Casuals" discord server</li>
     </ul>
 
-    <p>If you would like to contribute data, please contact Infi (@infi on Discord).</p>
+    <p>If you need help or have issues, please write in <a href="https://discord.com/channels/581875019861328007/1143510564165926992" rel="nofollow">Support Channel</a> [<a href="https://discord.gg/3NMcUV5" rel="nofollow" class="link-opacity-75 link-info"><img src="https://camo.githubusercontent.com/ac8298a8ee46ec0b13a2502a94b72b4f0ecc69e30447ad75f8eca2786a733cd2/68747470733a2f2f646973636f72646170702e636f6d2f6170692f6775696c64732f3538313837353031393836313332383030372f7769646765742e706e673f7374796c653d736869656c64" alt="Discord Shield" data-canonical-src="https://discordapp.com/api/guilds/581875019861328007/widget.png?style=shield" style="max-width: 100%;"></a>].</p>
 </div>
