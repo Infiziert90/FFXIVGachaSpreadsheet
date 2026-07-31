7d46fc999c35bcb42a1a4fd770780d284497e017
Infi <infiziert@protonmail.ch>
2026-07-31 23:37:12 +0200

    - Oops

--- website/src/routes/submarineShip/+page.ts
+++ website/src/routes/submarineShip/+page.ts
@@ -1,5 +1,4 @@
 ﻿import type { PageLoad } from './$types';
-import {loadMapping} from "$lib/loadHelpers";
 import {InitializeHelpers} from "$lib/sheets/sheetHelper";
 import {
     LoadSubExplorationSheet,
@@ -8,11 +7,12 @@ import {
     LoadSubRankSheet
 } from "$lib/sheets/simplifiedSheets";
 import {importCalculatedData} from "$lib/submarines/calculated";
+import {loadItemMapping} from "$lib/mappings";
 
 // @ts-ignore
 export const load: PageLoad = async ({ fetch }) => {
     console.log(`Loading all required data`)
-    let mappingPromise = loadMapping(fetch);
+    let mappingPromise = loadItemMapping(fetch);
     let mapPromise = LoadSubMapSheet(fetch);
     let rankPromise = LoadSubRankSheet(fetch);
     let partPromise = LoadSubPartSheet(fetch);
