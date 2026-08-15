b03443036ef999372109a78a5354d2f0bf95ca39
Infi <infiziert@protonmail.ch>
2026-08-15 16:10:37 +0200

    - Update data

---
+++ Export/SupabaseExporter/SupabaseExporter/Structures/Exports/FateReward.cs
@@ -0,0 +1,31 @@
+﻿namespace SupabaseExporter.Structures.Exports;
+
+public class FateReward
+{
+    public List<Territory> Territories = [];
+    
+    public class Territory
+    {
+        public uint Id;
+        public long Records;
+
+        public List<Type> Types = [];
+    }
+
+    public class Type
+    {
+        public byte Id;
+        public long Records;
+
+        public List<Fate> Fates = [];
+    }
+
+    public class Fate
+    {
+        public uint Id;
+        public long Records;
+        
+        public List<Reward> Rewards = [];
+        public List<Reward> AdditionalRewards = [];
+    }
+}
